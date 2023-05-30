05aaa6c3e1d731a419ecf1907646bd2093d37e6b
Egna Polo <info@egnapolo.com>
2023-05-30 18:13:42 +0200

    Initial Commit

---
+++ Await/AwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwaitApp.swift
+//  Await
+//
+//  Created by Egna Lizeth Polo Rubiano on 30/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
